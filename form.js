39293a35aabf71adf8898f0b3c4e9010b9b0e1a2
Mansi Saini <mansisaini26@gmail.com>
2021-04-10 01:07:10 -0700

    firebase works

--- form.js
+++ form.js
@@ -31,8 +31,7 @@ function signIn() {
   const promise = auth.signInWithEmailAndPassword(email.value, password.value);
   promise.catch((e) => alert(e.message));
   alert("Signed In");
-  window.location.pathname =
-    "D:/newwelfare-ai-main/newwelfare-ai-main/inner-page.html";
+    window.location.pathname = "http://127.0.0.1:5500/random.html";
     // Fix path after website
 }
 
